df1d8ee03d79830597574a5a22b6cf4da5ac7315
okupala <okuharakensuke@gmail.com>
2022-06-19 22:36:19 +0900

    Setup TypeScript for Storybook

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,15 +1,28 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    // TODO: Delete these when the time comes
+    // "../stories/**/*.stories.mdx",
+    // "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/*.stories.@(ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    // TODO: Delete these when the time comes
+    // reactDocgen: "react-docgen-typescript",
+    // reactDocgenTypescriptOptions: {
+    //   shouldExtractLiteralValuesFromEnum: true,
+    //   propFilter: (prop) =>
+    //     prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    // },
+  },
 }
---
+++ packages/ui/src/components/Button/Button.stories.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./Button"
+import { ComponentStory } from "@storybook/react"
+
+export default {
+  title: "SpitchButton",
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+}
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+export const Primary = Template.bind({})
+
+Primary.args = {
+  children: "hogehogefugafuga",
+}
